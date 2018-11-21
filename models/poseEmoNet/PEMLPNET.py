8e4b935db47ab6e015274f741d5b533da365b50f
jiaxinchenX <chenjiaxinx@gmail.com>
2018-11-21 16:49:19 +0400

    update PEMLPNET

--- models/poseEmoNet/PEMLPNET.py
+++ models/poseEmoNet/PEMLPNET.py
@@ -3,16 +3,29 @@ slim = tf.contrib.slim
 
 
 def PEMLPNET(input, numb_class, is_training, is_reuse, name=None):
-    net = slim.utils.convert_collection_to_dict(('none', 1))
-    net['input'] = input
-    net['mlp1'] = slim.fully_connected(
-        net['input'], 18, reuse=is_reuse, scope='mlp1' )
-    net['mlp2'] = slim.fully_connected(
-        net['mlp1'], 9, reuse=is_reuse, scope='mlp2' )
-    net['logits'] = slim.fully_connected(
-        net['mlp2'], numb_class, activation_fn=None, reuse=is_reuse, scope='logits')
-    net['prediction'] = tf.nn.softmax( net['logits'] )
-    return net
+    if not name == None:
+        with tf.variable_scope(name):
+            net = slim.utils.convert_collection_to_dict(('none', 1))
+            net['input'] = input
+            net['mlp1'] = slim.fully_connected(
+                net['input'], 18, reuse=is_reuse, scope='mlp1' )
+            net['mlp2'] = slim.fully_connected(
+                net['mlp1'], 9, reuse=is_reuse, scope='mlp2' )
+            net['logits'] = slim.fully_connected(
+                net['mlp2'], numb_class, activation_fn=None, reuse=is_reuse, scope='logits')
+            net['prediction'] = tf.nn.softmax( net['logits'] )
+            return net
+    else:
+        net = slim.utils.convert_collection_to_dict(('none', 1))
+        net['input'] = input
+        net['mlp1'] = slim.fully_connected(
+            net['input'], 18, reuse=is_reuse, scope='mlp1')
+        net['mlp2'] = slim.fully_connected(
+            net['mlp1'], 9, reuse=is_reuse, scope='mlp2')
+        net['logits'] = slim.fully_connected(
+            net['mlp2'], numb_class, activation_fn=None, reuse=is_reuse, scope='logits')
+        net['prediction'] = tf.nn.softmax(net['logits'])
+        return net
 
 def loss_entropy(predictions,labels, numb_classes):
 
